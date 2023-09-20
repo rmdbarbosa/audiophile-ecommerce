3a821f2708599c543853444e5161aab0b582308c
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-20 19:29:35 -0300

    feat: mobile home button styling / mobile home products section

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,33 @@
 import Button from "./components/Button";
+import ProductCard from "./components/menu/ProductCard";
 
 export default function Home() {
   return (
-    <div
-      className={`flex items-center text-center min-h-[600px] absolute  top-0 left-0 right-0 bg-cover bg-center bg-[url('/assets/home/mobile/image-header.jpg')]`}
-    >
-      <div className="flex flex-col items-center justify-center gap-6 min-h-[290px] px-6 pt-24">
-        <h2 className="text-gray-300 uppercase tracking-[.55em]">
-          New product
-        </h2>
-        <h1 className="text-white font-bold uppercase text-4xl ">
-          XX99 Mark II Headphones
-        </h1>
-        <p className="text-gray-300">
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <Button bgColor="bg-orange-400" textColor="white" hover="orange-300" />
+    <>
+      <div
+        className={`flex relative items-center text-center min-h-[600px] absolute  top-0 left-0 right-0 bg-cover bg-center bg-[url('/assets/home/mobile/image-header.jpg')]`}
+      >
+        <div className="flex flex-col items-center justify-center gap-6 min-h-[290px] px-6 pt-24">
+          <h2 className="text-gray-300 uppercase tracking-[.55em]">
+            New product
+          </h2>
+          <h1 className="text-white font-bold uppercase text-4xl ">
+            XX99 Mark II Headphones
+          </h1>
+          <p className="text-gray-300">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <Button
+            bgColor="bg-orange-400"
+            textColor="white"
+            hover="orange-300"
+          />
+        </div>
       </div>
-    </div>
+      <div className="absolute left-0 right-0 py-6 px-6 uppercase">
+        <ProductCard />
+      </div>
+    </>
   );
 }
