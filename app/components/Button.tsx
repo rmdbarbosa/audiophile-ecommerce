c0ec19e83dd7c3ba3f368a15aaafd145b6929565
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-20 19:00:03 -0300

    feat: mobile home hero header created/styling

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function Button({ bg }: any) {
+type ButtonProps = {
+  bgColor: string;
+  textColor: string;
+  hover: string;
+};
+
+export default function Button({ bgColor, textColor, hover }: ButtonProps) {
   return (
-    <div
-      className={`bg-white min-w-[160px] min-h-[48px] flex items-center justify-center`}
+    <button
+      className={`hover:bg-${hover} ${bgColor} text-${textColor} min-w-[160px] min-h-[48px] flex items-center justify-center`}
     >
       SEE PRODUCT
-    </div>
+    </button>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
     <div
       className={`flex items-center text-center min-h-[600px] absolute  top-0 left-0 right-0 bg-cover bg-center bg-[url('/assets/home/mobile/image-header.jpg')]`}
     >
-      <div className="flex flex-col items-center justify-center gap-6 min-h-[290px] px-6 pt-16">
+      <div className="flex flex-col items-center justify-center gap-6 min-h-[290px] px-6 pt-24">
         <h2 className="text-gray-300 uppercase tracking-[.55em]">
           New product
         </h2>
-        <h1 className="text-white font-bold uppercase text-4xl">
+        <h1 className="text-white font-bold uppercase text-4xl ">
           XX99 Mark II Headphones
         </h1>
         <p className="text-gray-300">
           Experience natural, lifelike audio and exceptional build quality made
           for the passionate music enthusiast.
         </p>
-        <Button bg={"orange"} />
+        <Button bgColor="bg-orange-400" textColor="white" hover="orange-300" />
       </div>
     </div>
   );
