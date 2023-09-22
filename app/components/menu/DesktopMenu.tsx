94c04e52f1f99e2366f7ea2900248b5b4ac0a14c
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-22 13:28:22 -0300

    feat: dynamic routes product category added

--- app/components/menu/DesktopMenu.tsx
+++ app/components/menu/DesktopMenu.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const links = ["home","headphone","speakers","earphones"]
+const links = ["home", "headphone", "speakers", "earphones"];
 
 export default function DesktopMenu() {
   return (
-    <ul className='hidden lg:flex lg:gap-6 text-white z-30 uppercase text-xs font-bold'>
-        {links.map(link => <li className='cursor-pointer hover:text-orange-400 transition-colors' key={link}>{link}</li>)}
+    <ul className="hidden lg:flex gap-6 text-white z-30 uppercase text-xs font-bold tracking-[0.4em]">
+      {links.map((link) => (
+        <li
+          className="cursor-pointer hover:text-orange-400 transition-colors"
+          key={link}
+        >
+          {link}
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
