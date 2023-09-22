560d2eebd2305f54bdb412079b4f348553fdbb57
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-09-21 21:32:48 -0300

    fix: desktop layout ManWithHeadphone component styling

--- app/components/home/ManWithHeadphone.tsx
+++ app/components/home/ManWithHeadphone.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ManWithHeadphone() {
   return (
-    <div className="flex flex-col  text-center lg:flex-row-reverse lg:items-center">
+    <div className="flex flex-col gap-8 lg:gap-0 text-center lg:flex-row-reverse lg:items-center">
       <div className="bg-cover bg-center bg-[url(/assets/shared/mobile/image-best-gear.jpg)] sm:bg-[url(/assets/shared/tablet/image-best-gear.jpg)] lg:bg-[url(/assets/shared/desktop/image-best-gear.jpg)] lg:min-h-[588px] min-h-[300px]  flex-1  rounded-xl mt-20"></div>
-      <div className="lg:flex lg:flex-col lg:justify-center  lg:max-h-[295px] flex-1">
+      <div className="lg:flex lg:flex-col lg:justify-center  lg:max-h-[295px]  flex-1">
         <h2 className="font-bold uppercase text-3xl lg:max-w-[300px] lg:text-left">
           Bringing you the <span className="text-orange-400">best</span> audio
           gear
