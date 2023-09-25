a7d7aa326daa5ff4b73ad8da115d007b841242c0
Rafael Barbosa <69510537+rmdbarbosa@users.noreply.github.com>
2023-09-25 12:56:30 -0300

    fix: CartContext logic addToCart

--- app/context/cart-context.tsx
+++ app/context/cart-context.tsx
@@ -1,16 +1,23 @@
 "use client"
 
-import {createContext, useContext, useState} from "react"
+import {createContext, useContext, useState, useEffect} from "react"
+
 
 export const CartContext = createContext<null | any>(null)
 
 export default function CartContextProvider({children}: any) {
     const [cartModal, setCartModal] = useState<any>(false)
-    const [cart, setCart] = useState(null)
+    const [cart, setCart] = useState<any>([])
 
     const addToCart = (item: any) => {
-        setCart(item)
+        const { id, cartImg, quantity, name } = item;
+        const cartItem = { id, cartImg, quantity, name };
+        setCart((prevCart: any) => [...prevCart, cartItem]);
     }
+
+    useEffect(() => {
+console.log(cart)
+    }, [cart] )
 
 return (
    <CartContext.Provider value={{cartModal, setCartModal, cart, addToCart}}>
